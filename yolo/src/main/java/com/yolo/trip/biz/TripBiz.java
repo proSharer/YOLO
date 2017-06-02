aa5ee2a210b32c71ae58d2f8917621678e14cf3c
이지수 <lizzysoo0000@gmail.com>
2017-06-02 15:05:27 +0900

    no message

--- yolo/src/main/java/com/yolo/trip/biz/TripBiz.java
+++
@@ -1,8 +0,0 @@
-package com.yolo.trip.biz;
-
-import com.yolo.trip.vo.TripVO;
-
-public interface TripBiz {
-	
-	public String addNewOneTrip(TripVO tripVO);
-}
--- yolo/src/main/java/com/yolo/trippart/biz/TripPartBiz.java
+++
@@ -1,9 +0,0 @@
-package com.yolo.trippart.biz;
-
-import com.yolo.trippart.vo.TripPartVO;
-
-public interface TripPartBiz {
-
-	public boolean addOneTripPart(TripPartVO tripPartVO);
-
-}
