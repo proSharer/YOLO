c1954bfe0a779549d666a0be2c719e17eadc3bf7
lizzyman <rowltn14@gmail.com>
2017-06-02 13:59:53 +0900

    no message

--- yolo/src/main/java/com/yolo/daily/dao/DailyDao.java
+++
@@ -1,9 +0,0 @@
-package com.yolo.daily.dao;
-
-import com.yolo.daily.vo.DailyVO;
-
-public interface DailyDao  {
-
-	
-	public int insertDailyPart(DailyVO dailyVO);
-}
--- yolo/src/main/java/com/yolo/daily/service/DailyService.java
+++
@@ -1,9 +0,0 @@
-package com.yolo.daily.service;
-
-import com.yolo.daily.vo.DailyVO;
-
-public interface DailyService {
-
-	
-	public boolean insertDailyPart(DailyVO dailyVO);
-}
