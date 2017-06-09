924d76d80cb4bfc16c165eb93ff9ea651f8d5045
Admin-PC\Admin <bluebus4727@gmail.com>
2017-06-09 17:12:28 +0900

    James

---
+++ yolo/src/main/java/com/yolo/user/service/UserServiceImpl.java
@@ -0,0 +1,63 @@
+package com.yolo.user.service;
+
+import com.yolo.user.biz.UserBiz;
+import com.yolo.user.dao.UserDao;
+import com.yolo.user.vo.UserVO;
+import com.yolo.common.utilities.SHA256Util;
+
+public class UserServiceImpl implements UserService {
+
+	private UserBiz userBiz;
+	private UserDao userDao;
+
+	public void setUserBiz(UserBiz userBiz) {
+		this.userBiz = userBiz;
+	}
+
+	public void setUserDao(UserDao userDao) {
+		this.userDao = userDao;
+	}
+
+	@Override
+	public boolean insertOneUser(UserVO userVO) {
+
+		String salt = SHA256Util.generateSalt();
+		userVO.setSalt(salt);
+
+		String password = userVO.getPassword();
+		password = SHA256Util.getEncrypt(password, salt);
+		userVO.setPassword(password);
+
+		userVO.setAuthId("USR");
+
+		return userBiz.insertOneUser(userVO);
+	}
+
+	@Override
+	public UserVO selectOneUser(UserVO userVO) {
+
+		if (userBiz.selectRockStatus(userVO.getUserId())) {
+			return null;
+		}
+		
+		userBiz.updateClearLoginFailCount(userVO.getUserId());
+		
+		String salt = userBiz.getSalt(userVO.getUserId());
+		
+		String password = userVO.getPassword();
+		password = SHA256Util.getEncrypt(password, salt);
+		userVO.setPassword(password);
+		
+		UserVO user = userBiz.selectOneUser(userVO);
+
+		if (user != null) {
+			userBiz.updateClearRockCount(userVO.getUserId());
+		} else {
+			userBiz.plusLoginFailCount(userVO.getUserId());
+			userBiz.updateRockStatus(userVO.getUserId());
+		}
+
+		return user;
+	}
+
+}
