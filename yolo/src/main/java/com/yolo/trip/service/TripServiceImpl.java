6fe72c697240aecc231ad39c62a9d9f365efb380
김정진 <rww4287@gmail.com>
2017-06-09 17:41:30 +0900

    ㅇㄹㄴㅇ

--- yolo/src/main/java/com/yolo/trip/service/TripServiceImpl.java
+++ yolo/src/main/java/com/yolo/trip/service/TripServiceImpl.java
@@ -1,6 +1,10 @@
 package com.yolo.trip.service;
 
+import java.util.List;
+
 import com.yolo.trip.biz.TripBiz;
+import com.yolo.trip.vo.TripListVO;
+import com.yolo.trip.vo.TripSearchVO;
 import com.yolo.trip.vo.TripVO;
 import com.yolo.trippart.biz.TripPartBiz;
 import com.yolo.trippart.vo.TripPartListVO;
@@ -30,6 +34,7 @@ public class TripServiceImpl implements TripService{
 		
 		for (int i = 0 ; i < size ; i++){
 			TripPartVO tripPartVO = new TripPartVO();
+			
 			tripPartVO.setTripId(tripId);
 			tripPartVO.setStartTime(tripPartListVO.getStartTime().get(i));
 			tripPartVO.setEndTime(tripPartListVO.getEndTime().get(i));
@@ -37,6 +42,7 @@ public class TripServiceImpl implements TripService{
 			tripPartVO.setPlace(tripPartListVO.getPlace().get(i));
 			tripPartVO.setContent(tripPartListVO.getContent().get(i));
 			tripPartVO.setMap(tripPartListVO.getMap().get(i));
+			tripPartVO.setRealFileName(tripPartListVO.getRealFileName().get(i));
 			
 			isSuccess = tripPartBiz.addOneTripPart(tripPartVO);
 			
@@ -44,5 +50,26 @@ public class TripServiceImpl implements TripService{
 		
 		
 		return isSuccess;
+	}
+	@Override
+	public TripListVO selectAllTrips(TripSearchVO tripSearchVO) {
+		return tripBiz.selectAllTrips(tripSearchVO);
+	}
+	@Override
+	public List<TripPartVO> selectTripPartByTripId(String tripId) {
+
+		return tripPartBiz.selectTripPartByTripId(tripId);
+	}
+	@Override
+	public boolean removeTrip(String tripId) {
+		return tripBiz.removeTrip(tripId);
+	}
+	@Override
+	public TripVO selectOneTrip(String tripId) {
+		return tripBiz.selectOneTrip(tripId);
+	}
+	@Override
+	public boolean tripLikeCountPlus(String tripId) {
+		return tripBiz.tripLikeCountPlus(tripId);
 	}
 }
