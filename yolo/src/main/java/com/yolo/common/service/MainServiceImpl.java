d8b0110ca9a68fa6f60ba43ca3695714388a8d0b
김정진 <rww4287@gmail.com>
2017-08-23 13:34:27 +0900

    no message

--- yolo/src/main/java/com/yolo/common/service/MainServiceImpl.java
+++ yolo/src/main/java/com/yolo/common/service/MainServiceImpl.java
@@ -45,7 +45,7 @@ public class MainServiceImpl implements MainService {
 			userVO = new UserVO();
 			userVO.setUserId("");
 		}
-		List<LikeVO> likeList = likeBiz.getAllLikesByUserID(userVO);
+		List<LikeVO> likeList = likeBiz.getAllLikesByUserID(userVO); // 내가좋아요한 리스트.
 		if ( likeList.size() == 0 ){
 			return null;
 		}
@@ -58,29 +58,29 @@ public class MainServiceImpl implements MainService {
 		for ( int i = 0 ; i < likeList.size(); i++) {
 			
 			like = new ArrayList<LikeVO>();	
-			String tripId = likeList.get(i).getTripId();
-			like = likeBiz.getLikeListByTripId(tripId);
+			String tripId = likeList.get(i).getTripId();// 내가좋아요한 tripId 
+			like = likeBiz.getLikeListByTripId(tripId); // 내가 좋아요한 tripId의 좋아요 list
 			
-			for ( int j = 0 ; j < like.size(); j++ ){
+			for ( int j = 0 ; j < like.size(); j++ ){ // 이리스트만큼 
 				
 				tripList = new ArrayList<TripVO>();
-				String userId = like.get(j).getUserId();
+				String userId = like.get(j).getUserId(); // 여기서 좋아요한 유저를 가져와서 
 
-				if ( !userId.equals(userVO.getUserId()) ) {
+				if ( !userId.equals(userVO.getUserId()) ) { // 유저가 지금 나와같지 않다면 
 					
-					UserVO user = new UserVO();
+					UserVO user = new UserVO(); 
 					user.setUserId(userId);
-					otherLike = likeBiz.getAllLikesByUserID(user);
+					otherLike = likeBiz.getAllLikesByUserID(user); // 그 또다른 유저가 좋아요한 목록을 가져와서 
 					
 					for ( int k = 0 ; k< otherLike.size(); k++ ){
 						
 						tripVO = new TripVO();
-						String trip = otherLike.get(k).getTripId(); 
-						tripVO = tripBiz.selectOneTrip(trip);
-						tripList.add(tripVO);
+						String trip = otherLike.get(k).getTripId(); // 그좋아요한 목록의 id 를가져와서
+						tripVO = tripBiz.selectOneTrip(trip); // 하나씩 가져와..
+						tripList.add(tripVO); // 그래서 리스트에 담아 
 						
 					}
-					recommendList.addAll(tripList);
+					recommendList.addAll(tripList); // 리스트 ++
 				} 
 				
 			}
